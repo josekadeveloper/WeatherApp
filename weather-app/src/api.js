4411374e5a2548c27e1ee7a5f7669c690abbe693
jockdeveloper <jockdeveloper@gmail.com>
2022-09-21 23:46:40 +0200

    Creando componente Search

---
+++ weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ee7ee841demsh65244e570d5cdcep15bc1ajsn985e18305e00',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
